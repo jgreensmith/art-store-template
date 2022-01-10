0e228503975f23d4eb9b05ea764c77a93117e76c
James Greensmith <jgreensmith13@gmail.com>
2022-01-10 20:19:24 +0000

    cart position absolute

--- components/shop/Cart.js
+++ components/shop/Cart.js
@@ -1,11 +1,14 @@
-import { Button, Divider, Grid, IconButton, Table, TableBody, TableCell, TableRow, Toolbar, Typography } from '@mui/material';
+import { Button, Divider, Grid, IconButton, Paper, Table, TableBody, TableCell, TableRow, Toolbar, Typography } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import dynamic from 'next/dynamic';
 import { Store } from '../../utils/Store';
 import { CartImg } from '../../utils/styles';
 import getCommerce from '../../utils/commerce';
 import { CART_RETRIEVE_SUCCESS } from '../../utils/constants';
+import NextLink from 'next/link';
+import { Box } from '@mui/system';
+
 
 function Cart(props) {
     const { state, dispatch } = useContext(Store);
@@ -17,6 +20,10 @@ function Cart(props) {
         const cartData = await commerce.cart.remove(lineItem.id);
         dispatch({ type: CART_RETRIEVE_SUCCESS, payload: cartData.cart });
     };
+
+    const procceedToCheckoutHandler = () => {
+
+    }
 
     return (
         <div>
@@ -67,6 +74,46 @@ function Cart(props) {
                     
                 </TableBody>
             </Table>
+            
+                <Paper sx={{ position: 'absolute', bottom: 0, right: 0, left: 0, padding: 3 }} elevation={3}>
+                    
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography>Subtotal: {cart.data?.subtotal.formatted_with_symbol}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            {cart.data?.total_items > 0 && (
+                                <Button
+                                    type="button"
+                                    fullWidth
+                                    variant="contained"
+                                    color="secondary"
+                                    onClick={procceedToCheckoutHandler}
+                                >
+                                    Proceed to checkout
+                                </Button>
+                            )}
+                        </Grid>
+                        <Grid item xs={6}>
+                            <NextLink href='/shop'>
+                                <Button
+                                    type="button"
+                                    fullWidth
+                                    variant="text"
+                                    color="secondary"
+                                >
+                                    Continue Shopping
+                                </Button> 
+                            </NextLink>
+                            
+                        </Grid>
+
+                    </Grid>
+                    
+                </Paper>
+            
+            
+
         </div>
     )
 }
